757f356fddfe14b8e98b265bc6a3bcc51cc09ae5
Rafsanfaruk <39384467+Rafsanfaruk@users.noreply.github.com>
2023-05-28 14:58:37 +0600

    My cart page implement

--- src/Pages/Dashboard/MyCart/MyCart.jsx
+++ src/Pages/Dashboard/MyCart/MyCart.jsx
@@ -3,19 +3,50 @@
 import { Helmet } from "react-helmet-async";
 import useCart from "../../../hooks/useCart";
 import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const MyCart = () => {
-  const [cart] = useCart();
+  const [cart, refetch] = useCart();
   console.log(cart);
-
+// how does reduce work
   const total = cart.reduce((sum, item) => item.price + sum, 0).toFixed(2);
 
+  const handleDelete = (item) => {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          fetch(`http://localhost:5000/carts/${item._id}`,{
+            method:'Delete',
+
+          })
+          .then(res => res.json())
+          .then(data => {
+            if(data.deletedCount > 0) {
+                refetch();
+                Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                  )
+            }
+          })
+        }
+      })
+  }
+
   return (
-    <div>
+    <div className="full">
       <Helmet>
         <title>Bistro Boss | My Cart </title>
       </Helmet>
-      <div className="uppercase font-semibold h-[60px] flex justify-evenly items-center">
+      <div className="uppercase font-semibold h-[60px] flex justify-evenly items-center mb-5">
         <h3 className="text-3xl">Total Items: {cart.length}</h3>
         <h3 className="text-3xl">Total Price: ${total}</h3>
         <button className="btn btn-sm btn-warning">PAY</button>
@@ -52,7 +83,7 @@ const MyCart = () => {
                 </td>
                 <td className="text-end">${item.price}</td>
                 <td>
-                  <button className="btn btn-ghost btn-lg bg-red-500 text-white"><FaTrashAlt></FaTrashAlt></button>
+                  <button onClick={()=> handleDelete(item)} className="btn btn-ghost bg-red-500 text-white"><FaTrashAlt></FaTrashAlt></button>
                 </td>
               </tr>
             ))}
